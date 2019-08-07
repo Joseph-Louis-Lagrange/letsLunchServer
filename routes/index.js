4f1f790ac1f4791f06458143e83d35e16b312eb5
Joseph-Louis Lagrange <joseph.louis.lagrange.999@gmail.com>
2019-08-07 15:42:54 +0000

    saraf et hatost

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
+var soda = require('../dal/soda.js');
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:docType', async function(req, res, next) {
+  var result = await soda.findAll(req.params.docType);
+  res.send(result);
+});
+
+router.get('/:docType/:id', async function(req, res, next) {
+  var result = await soda.findById(req.params.id, req.params.docType);
+  if(result){
+    res.send(result);
+  } else {
+    res.statusCode = 404;
+    res.send();
+  }
+});
+
+router.post('/:docType', async function(req, res) {
+  var result = await soda.insertDocument(req.body, req.params.docType);
+  if(result){
+    res.statusCode == 201;
+  }
+  res.send(result);
+})
+
+router.put('/:docType/:id', async function(req, res, next) {
+  var result = await soda.updateDocument(req.params.id, req.body, req.params.docType);
+  if(result){
+    res.send(result);
+  } else {
+    res.statusCode = 404;
+    res.send();
+  }
+});
+
+router.delete('/:docType/:id', async function(req, res, next) {
+  var result = await soda.deleteById(req.params.id, req.params.docType);
+  if(result){
+    res.send(result);
+  } else {
+    res.statusCode = 404;
+    res.send();
+  }
 });
 
 module.exports = router;
